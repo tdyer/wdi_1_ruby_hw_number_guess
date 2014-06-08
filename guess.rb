21216e96a68f4b8b482335767b0b670d314e70f5
Alex Grant <grantovich@users.noreply.github.com>
2014-06-08 18:55:15 -0400

    Bonus: User can choose to play the game again

--- guess.rb
+++ guess.rb
@@ -1,4 +1,4 @@
-puts 'Welcome to Number Guesser Pro 2003 Service Pack 1'
+puts 'Welcome to Number Guesser Pro 2003 Service Pack 2'
 puts
 puts 'Enter the maximum number to guess:'
 
@@ -8,35 +8,42 @@ until max_number >= 10
   max_number = gets.chomp.to_i
 end
 
-puts
-puts "I'm thinking of a number between 1 and #{max_number}..."
+continue_playing = true
 
-secret_number = rand(max_number) + 1
-guesses_left = Math.log(max_number * 3).round
-guess = 0
-
-while guess != secret_number && guesses_left > 0
+while continue_playing
   puts
+  puts "I'm thinking of a number between 1 and #{max_number}..."
 
-  case
-  when guess == 0 then puts 'Guess the number:'
-  when guess < secret_number then puts "It's higher than #{guess}, guess again:"
-  when guess > secret_number then puts "It's lower than #{guess}, guess again:"
-  end
+  secret_number = rand(max_number) + 1
+  guesses_left = Math.log(max_number * 3).round
+  guess = 0
 
-  puts "(#{guesses_left} guesses left)"
+  while guess != secret_number && guesses_left > 0
+    puts
 
-  guess = gets.chomp.to_i
-  until guess >= 1 && guess <= max_number
-    puts "The number is between 1 and #{max_number}! Try again:"
+    case
+    when guess == 0 then puts 'Guess the number:'
+    when guess < secret_number then puts "It's higher than #{guess}, guess again:"
+    when guess > secret_number then puts "It's lower than #{guess}, guess again:"
+    end
+
+    puts "(#{guesses_left} guesses left)"
+
     guess = gets.chomp.to_i
+    until guess >= 1 && guess <= max_number
+      puts "The number is between 1 and #{max_number}! Try again:"
+      guess = gets.chomp.to_i
+    end
+    guesses_left -= 1
   end
-  guesses_left -= 1
-end
 
-puts
-if guess == secret_number
-  puts 'You got it! Congratulations!'
-else
-  puts "Nice try, but the number was #{secret_number}."
+  puts
+  if guess == secret_number
+    puts 'You got it! Congratulations!'
+  else
+    puts "Nice try, but the number was #{secret_number}."
+  end
+
+  puts 'Enter "y" to play again or "n" to quit:'
+  continue_playing = (gets.chomp.downcase == 'y')
 end
