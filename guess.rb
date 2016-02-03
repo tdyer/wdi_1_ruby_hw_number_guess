917243983ca741672018c63ba2cc53f576f1f488
lpond22 <laura@thelevelup.com>
2016-02-03 14:06:19 -0500

    Removed some instances of 'tries += 1' that were not working.

--- guess.rb
+++ guess.rb
@@ -3,23 +3,28 @@ number = rand(1...10)
 guess = 9
 tries = 1
 max_tries = 3
+max_number = 11
 
 while guess != number
   puts "Enter a number from 1 to 10: "
   guess = gets.chomp.to_i
-while guess < number
-  puts "You guessed too low! Try again: "
-  guess = gets.chomp.to_i
-while guess > number
-  puts "You guessed too high! Try again: "
-  guess = gets.chomp.to_i
-  unless tries < max_tries
-    puts "Sorry, you've exceeded the number of tries. The correct number is #{number}"
-    break
+  while guess < number
+    puts "You guessed too low! Try again: "
+    guess = gets.chomp.to_i
+    while guess > number
+      puts "You guessed too high! Try again: "
+      guess = gets.chomp.to_i
+      while guess > max_number
+        puts "You guessed outside the range. Enter a number from 1 to 10: "
+        guess = gets.chomp.to_i
+        unless tries < max_tries
+          puts "Sorry, you've exceeded the number of tries. The correct number is #{number}"
+          break
+        end
+        tries += 1
+      end
+    end
   end
-  tries += 1
-end
-end
 end
 
 puts "You guessed right!!" if guess == number
