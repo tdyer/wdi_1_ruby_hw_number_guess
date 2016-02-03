01bd053e86663fd5730198311cd604613461299a
lpond22 <laura@thelevelup.com>
2016-02-02 20:04:32 -0500

    First attempt at adding in a loop for letting using know if their guess is too high or low

--- guess.rb
+++ guess.rb
@@ -7,10 +7,17 @@ max_tries = 4
 while guess != number
   puts "Enter a number from 1 to 10: "
   guess = gets.chomp.to_i
+while guess < number
+  puts "You guessed too low! Try again: "
+  guess = gets.chomp.to_i
+while guess > number
+  puts "You guessed too high! Try again: "
+  guess = gets.chomp.to_i
   unless tries < max_tries
     puts "Sorry, you've exceeded the number of tries"
     break
   end
   tries += 1
 end
-
+end
+end
