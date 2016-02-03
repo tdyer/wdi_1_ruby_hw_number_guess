d4dee1eb27d316de81b90f419f72aa2715cbb0fa
lpond22 <laura@thelevelup.com>
2016-02-03 14:28:19 -0500

    Changed how to get guessed number (so it's not hardcoded to start. Also removed the first 'while' clause. Tries += 1 now works as intended.

--- guess.rb
+++ guess.rb
@@ -1,30 +1,32 @@
 # Your code goes here
 number = rand(1...10)
-guess = 9
 tries = 1
 max_tries = 3
 max_number = 11
 
-while guess != number
-  puts "Enter a number from 1 to 10: "
+puts "Enter a number from 1 to 10: "
+guess = gets.chomp.to_i
+
+
+while guess < number
+  puts "You guessed too low! Try again: "
   guess = gets.chomp.to_i
-  while guess < number
-    puts "You guessed too low! Try again: "
+  tries += 1
+  while guess > number
+    puts "You guessed too high! Try again: "
     guess = gets.chomp.to_i
-    while guess > number
-      puts "You guessed too high! Try again: "
+    tries += 1
+    while guess > max_number
+      puts "You guessed outside the range. Enter a number from 1 to 10: "
       guess = gets.chomp.to_i
-      while guess > max_number
-        puts "You guessed outside the range. Enter a number from 1 to 10: "
-        guess = gets.chomp.to_i
-        unless tries < max_tries
-          puts "Sorry, you've exceeded the number of tries. The correct number is #{number}"
-          break
-        end
-        tries += 1
-      end
+      tries += 1
     end
   end
+  unless tries < max_tries
+    puts "Sorry, you've exceeded the number of tries. The correct number is #{number}"
+    break
+  end
 end
+
 
 puts "You guessed right!!" if guess == number
