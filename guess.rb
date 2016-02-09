1d956d83e88e663b253e300174db4bd2015439b1
lpond22 <laura@thelevelup.com>
2016-02-08 19:27:41 -0500

    Changed the while loop to a while/if/elsif/else loop.

--- guess.rb
+++ guess.rb
@@ -1,33 +1,24 @@
 # Your code goes here
-number = rand(1...10)
+number = rand(1...10) + 1
 #guess = 11
 tries = 1
 max_tries = 3
 max_number = 11
 
-print "Enter a number from 1 to 10: "
-guess = gets.chomp!.to_i
+while tries < 3
+    puts 'Enter a number from 1 to 10:'
+    guess = gets.chomp
+    guess = guess.to_i
+    tries = tries + 1
 
-while guess < number
-  tries += 1
-  puts "You guessed too low! Try again: "
-  guess = gets.chomp!.to_i
-
-  while guess > number
-    tries += 1
-    puts "You guessed too high! Try again: "
-    guess = gets.chomp!.to_i
-
-    while guess > max_number
-      puts "You guessed outside the range. Enter a number from 1 to 10: "
-      guess = gets.chomp!.to_i
+    if guess < number
+        puts "#{guess} is too low. Try again."
+    elsif guess > number
+        puts "#{guess} is too high. Try again."
+    else guess == number
+        puts "Great! You guessed my number."
+        break
     end
-  end
-  unless tries < max_tries
-    puts "Sorry, you've exceeded the number of tries. The correct number is #{number}"
-    break
-  end
-  tries += 1
 end
 
 puts "You guessed right!!" if guess == number
